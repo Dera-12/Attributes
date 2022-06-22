0172d61c9bbc8ed00b05b36111395e2c8d11e582
chidera Jessica <jescachidera@gmail.com>
2022-06-22 23:36:19 +0100

    first commit

---
+++ rod.js
@@ -0,0 +1,15 @@
+let full_name = "Ezeh Chidera Jessica";
+
+   // full name
+
+
+
+let height = "6.5ft tall";
+// height
+
+let Nationality = "Nigeria is my country"
+// country
+
+window.alert( full_name +"\n"+ height+"\n" + Nationality)
+
+console.log( full_name + "\n  \n" + height +"\n \n" + Nationality +  "is my country");
